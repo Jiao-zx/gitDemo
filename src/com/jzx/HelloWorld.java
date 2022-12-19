d9f3b8824ff122352cbe43f76eba16ebf5111156
Jiao-zx <82350084+Jiao-zx@users.noreply.github.com>
2022-12-19 19:03:51 +0800

    Update HelloWorld.java
    add lines

--- src/com/jzx/HelloWorld.java
+++ src/com/jzx/HelloWorld.java
@@ -3,5 +3,7 @@ package com.jzx;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("hello world");
+        System.out.printf("hello world");
+        System.out.printf("hello world");
     }
 }
