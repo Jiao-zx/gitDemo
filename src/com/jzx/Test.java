94d972e29ee4c63df1df91fe1d55b0dcf90932fd
Jiao-zx <82350084+Jiao-zx@users.noreply.github.com>
2022-12-19 19:56:21 +0800

    Update Test.java
    xiaoming update

--- src/com/jzx/Test.java
+++ src/com/jzx/Test.java
@@ -3,5 +3,6 @@ package com.jzx;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test branch");
+        System.out.println("test branch xiaoming");
     }
 }
