0ae23556bb457e2de7115c9c21d2ed2eb741afb2
jzx <jzx6152@163.com>
2022-12-19 19:18:58 +0800

    local add

--- src/com/jzx/HelloWorld.java
+++ src/com/jzx/HelloWorld.java
@@ -4,7 +4,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("hello world");
         System.out.println("hello local");
-        System.out.println("hello local");
-        System.out.println("hello local");
     }
 }
